4c63a6357df6ba08abc84ccc95d60b554c817601
BADLZ <48454150+BADLZ@users.noreply.github.com>
2019-04-26 15:44:06 +0100

    img

--- Sockets-ICD/src/Graphics/Scenes/LoginWindow.java
+++ Sockets-ICD/src/Graphics/Scenes/LoginWindow.java
@@ -13,12 +13,12 @@ import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
+import javax.swing.SwingConstants;
 
-import Client.Aluno.ClienteAluno;
 import Graphics.SceneManager;
 import Graphics.Aesthetics.FancyButton;
 import Graphics.Aesthetics.FancyTextField;
-import Server.HandleConnectionThread;
+import Server.DocumentLoader;
 import Server.Login;
 
 public class LoginWindow extends JLabel {
@@ -26,13 +26,15 @@ public class LoginWindow extends JLabel {
 	private static final long serialVersionUID = 1L;
 
 	private SceneManager sm;
+	private DocumentLoader docLoad;
 
-	private JTextArea error;
+	private JLabel error;
 	private JTextField numberfield;
-	private ImageIcon loginBtnimg, loginBtnpressedimg, textfieldimg;
+	private ImageIcon loginBtnimg, loginBtnpressedimg, btnVoltarimg, btnVoltarpressedimg, textfieldimg;
 
 	public LoginWindow(SceneManager sm) {
 		this.sm = sm;
+		docLoad = new DocumentLoader();
 		initialize();
 	}
 
@@ -41,35 +43,33 @@ public class LoginWindow extends JLabel {
 		try {
 			loginBtnimg = new ImageIcon(ImageIO.read(new File("src/Images/loginBtn.png")));
 			loginBtnpressedimg = new ImageIcon(ImageIO.read(new File("src/Images/loginBtnpressed.png")));
+			btnVoltarimg = new ImageIcon(ImageIO.read(new File("src/Images/voltarBtn.png")));
+			btnVoltarpressedimg = new ImageIcon(ImageIO.read(new File("src/Images/voltarBtnpressed.png")));
 			textfieldimg = new ImageIcon(ImageIO.read(new File("src/Images/textfieldimg.png")));
+		
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 
 		setIcon(sm.getBackground());
-		numberfield = new FancyTextField("Enter Your Number", sm.screenWidth / 2 - 121, sm.screenHeight / 2 - 73, 240,
+		numberfield = new FancyTextField("Enter Your Number", sm.screenWidth / 2 - 95, sm.screenHeight / 2 - 73, 195,
 				30);
 		add(numberfield);
 
-		error = new JTextArea();
-		error.setBounds(sm.screenWidth / 2 - 121, sm.screenHeight / 2 - 150, 240,
-				30);
-		Font font = new Font("Consolas", Font.PLAIN, 12);
+		error = new JLabel("",SwingConstants.CENTER);
+		Font font = new Font("Consolas", Font.BOLD, 12);
 		error.setFont(font);
 		error.setForeground(Color.red);
-		
-		error.setBounds(sm.screenWidth / 2 - 121, sm.screenHeight / 2 - 150, 240, 30);
+		error.setBounds(sm.screenWidth / 2 - 121, sm.screenHeight / 2 - 15, 270, 30);
 		add(error);
-		
-		
+
 		JLabel img1 = new JLabel(textfieldimg);
 		img1.setBounds(sm.screenWidth / 2 - 210, sm.screenHeight / 2 - 135, 420, 150);
 		add(img1);
 
-		FancyButton btnLogin = new FancyButton("btnLogin", sm.screenWidth / 2 - 100, sm.screenHeight / 2, 200, 67,
+		FancyButton btnLogin = new FancyButton("btnLogin", sm.screenWidth / 2 - 100, sm.screenHeight / 2 + 20, 200, 67,
 				loginBtnimg, loginBtnpressedimg);
 
-		
 		btnLogin.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent arg0) {
@@ -84,10 +84,20 @@ public class LoginWindow extends JLabel {
 			}
 		});
 		add(btnLogin);
+		
+		FancyButton btnVoltar = new FancyButton("btnVoltar", sm.screenWidth / 2 - 100, sm.screenHeight / 2 + 20, 200, 67,
+				loginBtnimg, loginBtnpressedimg);
 	}
 
 	private void tryLogin() {
+		String s = numberfield.getText();
+		if (s != null && Login.alunoExiste(docLoad.getAlunosDoc(), s)) {
 
+		} else {
+			
+			error.setText("O número que introduziu não é válido");
+			error.setOpaque(true);
+		}
 	}
 
 }
